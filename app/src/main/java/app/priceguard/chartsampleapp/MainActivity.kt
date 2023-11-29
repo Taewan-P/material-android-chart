f8bc16668d6144c82336c80f2a2eedec4ad49a2e
ootr47 <ootr45@gmail.com>
2023-11-29 17:16:49 +0900

    feat: update dummy data

--- app/src/main/java/app/priceguard/chartsampleapp/MainActivity.kt
+++ app/src/main/java/app/priceguard/chartsampleapp/MainActivity.kt
@@ -10,14 +10,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Chart>(R.id.example_chart_1).dataset = ExampleDataset(
             showXAxis = true, showYAxis = false, touchListener = { v, _ -> v.performClick() }, data = listOf(
-                ExampleData(1F, 10F),
-                ExampleData(2F, 1F),
-                ExampleData(3F, 3F),
-                ExampleData(4F, 7F),
-                ExampleData(5F, 8F),
-                ExampleData(6F, 4F),
-                ExampleData(7F, 2F),
-                ExampleData(8F, 8F)
+                ExampleData(1F, 11000F),
+                ExampleData(2F, 11000F),
+                ExampleData(3F, 13000F),
+                ExampleData(4F, 12000F),
+                ExampleData(5F, 11000F),
+                ExampleData(6F, 14000F),
+                ExampleData(7F, 12000F),
+                ExampleData(8F, 11000F),
+                ExampleData(9F, 11000F)
             )
         )
         findViewById<Chart>(R.id.example_chart_2).dataset = ExampleDataset(
@@ -30,7 +31,8 @@ class MainActivity : AppCompatActivity() {
                 ExampleData(16F, 8F),
                 ExampleData(18F, 4F),
                 ExampleData(24F, 2F),
-                ExampleData(25F, 8F)
+                ExampleData(25F, 8F),
+                ExampleData(26F, 8F)
             )
         )
     }
