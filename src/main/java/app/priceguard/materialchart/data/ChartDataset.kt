c2e7e114143cb38510a2662c5f362c5ffd176e53
EunhoKang <zay0njugin@gmail.com>
2023-11-30 22:31:29 +0900

    feat: dataset changed

--- src/main/java/app/priceguard/materialchart/data/ChartDataset.kt
+++ src/main/java/app/priceguard/materialchart/data/ChartDataset.kt
@@ -3,6 +3,7 @@ package app.priceguard.materialchart.data
 interface ChartDataset {
     val showXAxis: Boolean
     val showYAxis: Boolean
+    val isInteractive: Boolean
     val graphMode: GraphMode
     val data: List<ChartData>
     val gridLines: List<GridLine>
