e5c8ca27135082874e0f78f565c68e968e2aba30
EunhoKang <zay0njugin@gmail.com>
2023-11-30 21:07:26 +0900

    feat: add valid parameter

--- app/src/main/java/app/priceguard/chartsampleapp/ExampleData.kt
+++ app/src/main/java/app/priceguard/chartsampleapp/ExampleData.kt
@@ -2,4 +2,4 @@ package app.priceguard.chartsampleapp
 
 import app.priceguard.materialchart.data.ChartData
 
-data class ExampleData(override val x: Float, override val y: Float) : ChartData
+data class ExampleData(override val x: Float, override val y: Float, override val valid: Boolean) : ChartData
