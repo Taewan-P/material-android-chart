94d2688582daffd2154ef65b2a547b5fffb491ba
ootr47 <ootr45@gmail.com>
2023-11-30 14:47:06 +0900

    refactor: Remove unused property

--- src/main/java/app/priceguard/materialchart/data/ChartDataset.kt
+++ src/main/java/app/priceguard/materialchart/data/ChartDataset.kt
@@ -1,10 +1,7 @@
 package app.priceguard.materialchart.data
 
-import android.view.View.OnTouchListener
-
 interface ChartDataset {
     val showXAxis: Boolean
     val showYAxis: Boolean
-    val touchListener: OnTouchListener?
     val data: List<ChartData>
 }
