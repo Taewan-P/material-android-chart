bddd98d8284f5e214ec3f25247a22bd577ea86d7
Taewan Park <swimingtw@gmail.com>
2024-02-05 16:20:49 +0900

    refactor: Update material library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,5 +147,5 @@ signing {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
